76ccc41077d72c05cd3e4b7a7425c4ed28e4f4ab
dave_dow <dow4537@gmail.com>
2020-12-02 10:58:59 +0000

    footer hidden

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,15 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      {/* <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a> */}
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
-        <span>
+        {/* <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
           <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
+        </span> */}
       </div>
     </footer>
   </div>
