17b2c6397eb506cb950b80b5ba5da31c9b81677f
dave_dow <dow4537@gmail.com>
2021-03-12 17:09:50 +0000

    form update

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -135,9 +135,9 @@ class Form extends React.Component {
               <option disabled hidden>
                 Type of Enquiry
               </option>
-              <option>Need to know more</option>
+              <option>Need to know more!</option>
               {/* <option>Found a bug</option> */}
-              <option>Want to say hello</option>
+              <option>Just want to say hello</option>
             </select>
           </label>
           <label className="Form--Label">
