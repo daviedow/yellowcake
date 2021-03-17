1a9af8da9306cf6d69fa99bf2778e5f0d7f130c2
dave_dow <dow4537@gmail.com>
2021-03-17 11:24:15 +0000

    name change

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'Melgo',
+        short_name: 'Melgo',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
