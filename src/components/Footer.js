8e94eda1fe2a9696341fc76bbb3418f5171634ff
dave_dow <dow4537@gmail.com>
2021-04-14 17:34:18 +0100

    instagram

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://www.instagram.com/melgo.co/">@melgo.co</a>
+      <a href="https://www.instagram.com/melgo.co/">Instagram</a>
     </h2>
     <br />
     <footer className="footer">
