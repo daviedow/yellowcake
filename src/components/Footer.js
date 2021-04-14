c4ed0d1486b6aa727730f30291eaf55b5710ddb1
dave_dow <dow4537@gmail.com>
2021-04-14 17:08:48 +0100

    insta

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      {/* <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a> */}
+      <a href="https://www.instagram.com/melgo.co/">@melgo.co</a>
     </h2>
     <br />
     <footer className="footer">
